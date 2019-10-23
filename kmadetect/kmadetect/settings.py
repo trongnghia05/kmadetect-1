c00800a2c02e05b140efd9560071c3b99d301b2a
DucTrungNguyen98 <ductrungnguyen98@gmail.com>
2019-10-23 10:37:26 +0700

    fix venv

--- kmadetect/kmadetect/settings.py
+++ kmadetect/kmadetect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bsw5depbpsor3^y5mbn^!9f6o1k)vp7)obu-asvc=%fm%#rz_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- kmadetect/kmadetect/wsgi.py
+++ kmadetect/kmadetect/wsgi.py
@@ -7,9 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+
+import sys, os
+import site
 
 from django.core.wsgi import get_wsgi_application
+INTERP = "/home/ubuntu/local/bin/python"
+# INTERP is present twice so that the new python interpreter
+# knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/kmadetect/kmadetect')  # You must add your project here
+
+# sys.path.insert(0, cwd + '/my_project/bin')
+sys.path.insert(0, cwd + '/kmadetect/venv/lib/python3.6/site-packages')
+
+# Activate your virtual env
+# activate_env=os.path.expanduser("~/.virtualenvs/myprojectenv/bin/activate_this.py")
+activate_env=os.path.expanduser("~/kmadetect/venv/bin/activate")
+exec(activate_env, dict(__file__=activate_env))
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kmadetect.settings')
 
